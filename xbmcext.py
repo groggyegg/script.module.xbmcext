d31563a73b385ff4e12a61b966e53710f301fc1d
groggyegg <groggyegg@protonmail.com>
2023-01-17 19:52:42 +0100

    Add missing definition doc

--- xbmcext.py
+++ xbmcext.py
@@ -55,7 +55,9 @@ class DateTime(datetime.datetime):
         string, formats -> new DateTime parsed from a string.
 
         :param date_string: A string that contains a date and/or time.
+        :type date_string: str
         :param formats: Formats for matching the string.
+        :type formats: str
         :return: Returns a DateTime, or None if no match.
         :rtype: DateTime | None
         """
